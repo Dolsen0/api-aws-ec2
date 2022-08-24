be7e004a6933571575e4ceb9a7993bc42aaf18ba
dolsen0 <derek.olsen@gmail.com>
2022-08-24 16:42:27 -0400

    initialized api on aws with ec2

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = 5050
+
+app.get("/", (req,res) =>{
+    res.send("my api")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port `, PORT)
+})
